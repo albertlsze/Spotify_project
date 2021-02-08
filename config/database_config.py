6fb052a9e46c76195b4c5f7b8269dc202ad96b16
Albert Sze <60682664+albertlsze@users.noreply.github.com>
2021-02-08 17:45:32 -0500

    adding basic database_config file

---
+++ config/database_config.py
@@ -0,0 +1,15 @@
+''' Configurations for application'''
+
+sql_config = {
+    'username': '',
+    'password': '',
+    'host':'',
+    'port': '',
+    'database': ''
+}
+
+spotify_api_config = {
+    "client_id": "",
+    "client_secret": "",
+    "redirect_url": ""
+}
