258189f11243365dc51faeacdb768745b1cafc0a
Albert Sze <you@example.com>
2021-02-18 00:25:52 -0500

    update to prepopulate file

--- scripts/prepopulate.py
+++ scripts/prepopulate.py
@@ -1,28 +1,31 @@
 from config.database_config import sql_config
+from lib.models.declarative_base import DeclarativeBase
 from lib.connection.mysql import MYSQLConnection
-from lib.schemas.yeardb import YearDB
-from lib.schemas.genredb import GenreDB
-from lib.schemas.artistdb import ArtistDB
-from lib.schemas.songdb import SongDB
-from lib.schemas.musickeydb import MusicKeyDB
+from lib.schemas.musickeydb import MusicKeySchema
+from lib.schemas.artistdb import ArtistSchema
+from lib.schemas.genredb import GenreSchema
+from lib.schemas.songdb import SongSchema
+from lib.schemas.yeardb import YearSchema
 import lib.utils.Data_pipeline.prepopulate_db as prepopulate
-import os
 
 SQL_cnx = MYSQLConnection(sql_config)
-SQL_cnx.close()
 sql_tables = {
-    'musickey': MusicKeyDB(),
-    'genre' : GenreDB(),
-    'artist' : ArtistDB(),
-    'song' : SongDB(),
-    'year' : YearDB()
+    'musickey': MusicKeySchema(),
+    'genre' : GenreSchema(),
+    'artist' : ArtistSchema(),
+    'song' : SongSchema(),
+    'year' : YearSchema()
 }
 
-for key in sql_tables:
-    prepopulate.create_table(SQL_cnx,sql_tables[key])
+DeclarativeBase.metadata.drop_all(SQL_cnx.engine)
+DeclarativeBase.metadata.create_all(SQL_cnx.engine)
 
+#for key in sql_tables:
+#    prepopulate.create_table(SQL_cnx,sql_tables[key])
+'''
 print('\n')
 prepopulate.prepopulate_musicdb(SQL_cnx, sql_tables['musickey'])
 #prepopulate.prepopulate_yeardb(SQL_cnx, sql_tables['year'])
 
 SQL_cnx.close()
+'''
