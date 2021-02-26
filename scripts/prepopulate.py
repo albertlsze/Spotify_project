de653948281bf6d0d2b7a3d506b4f25392b957f0
Albert Sze <you@example.com>
2021-02-26 11:32:04 -0500

    update prepopulate script

--- scripts/prepopulate.py
+++ scripts/prepopulate.py
@@ -1,25 +1,31 @@
-from config.database_config import sql_config
+from config.database_config import configs
 from lib.models.declarative_base import DeclarativeBase
-from lib.connection.mysql import MYSQLConnection
+from lib.connection.mysqlcnx import MYSQLConnection
 from lib.schemas.musickeydb import MusicKeySchema
 from lib.schemas.artistdb import ArtistSchema
 from lib.schemas.genredb import GenreSchema
 from lib.schemas.songdb import SongSchema
 from lib.schemas.yeardb import YearSchema
+from lib.models.musickeydb import MusicKeyDB
+from lib.models.genredb import GenreDB
+from lib.models.artistdb import ArtistDB
+from lib.models.songdb import SongDB
+from lib.models.yeardb import YearDB
 import lib.utils.Data_pipeline.prepopulate_db as prepopulate
 
-SQL_cnx = MYSQLConnection(sql_config)
+SQL_cnx = MYSQLConnection(configs['sql'])
 sql_tables = {
-    'musickey': MusicKeySchema(),
-    'genre' : GenreSchema(),
-    'artist' : ArtistSchema(),
-    'song' : SongSchema(),
-    'year' : YearSchema()
+    'musickey': {'schema':MusicKeySchema(),'obj':MusicKeyDB()},
+    'genre' : {'schema':GenreSchema(),'obj':GenreDB()},
+    'artist' : {'schema':ArtistSchema(),'obj':ArtistDB()},
+    'song' : {'schema':SongSchema(),'obj':SongDB()},
+    'year' : {'schema':YearSchema(),'obj':YearDB()}
 }
 
 DeclarativeBase.metadata.drop_all(SQL_cnx.engine)
 DeclarativeBase.metadata.create_all(SQL_cnx.engine)
 
+prepopulate.prepopulate_db(SQL_cnx, sql_tables)
 '''
 print('\n')
 prepopulate.prepopulate_musicdb(SQL_cnx, sql_tables['musickey'])
