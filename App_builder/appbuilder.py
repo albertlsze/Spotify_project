37c870e74a1fcd3870fa29ebddf23e68f73e3362
Albert Sze <asze@bu.edu>
2021-03-08 08:27:31 -0500

    updates to App_builder folder

--- App_builder/appbuilder.py
+++ App_builder/appbuilder.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template,url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from config.database_config import sql_config
+from lib
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{sql_config['username']}:{sql_config['password']}@{sql_config['host']}/{sql_config['database']}"
+db = SQLAlchemy(app)
+
+class Todo(db.model,)
 
 @app.route('/')
 def index():
