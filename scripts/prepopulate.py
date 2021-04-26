223a988cb3b4d90b7f866804f8c7f3dac99202d3
Albert Sze <you@example.com>
2021-04-25 22:07:21 -0400

    update prepoulate script

--- scripts/prepopulate.py
+++ scripts/prepopulate.py
@@ -1,29 +1,29 @@
-from config.database_config import sql_config
+from config.database_config import POSTGRES_CONFIG
 from lib.models.declarative_base import DeclarativeBase
-from lib.connection.mysqlcnx import MYSQLConnection
+from lib.connection.postgres import PostgresConnection
 from lib.schemas.musickeydb import MusicKeySchema
 from lib.schemas.artistdb import ArtistSchema
 from lib.schemas.genredb import GenreSchema
 from lib.schemas.songdb import SongSchema
+from lib.schemas.song_timecale_db import SongTimeSchema
 from lib.schemas.yeardb import YearSchema
 from lib.models.musickeydb import MusicKeyDB
 from lib.models.genredb import GenreDB
 from lib.models.artistdb import ArtistDB
 from lib.models.songdb import SongDB
+from lib.models.song_timescale_db import SongTimeDB
 from lib.models.yeardb import YearDB
 import lib.utils.Data_pipeline.prepopulate_db as prepopulate
 
-SQL_cnx = MYSQLConnection(sql_config)
+SQL_cnx = PostgresConnection(POSTGRES_CONFIG)
 sql_tables = {
     'musickey': {'schema':MusicKeySchema(),'obj':MusicKeyDB()},
     'genre' : {'schema':GenreSchema(),'obj':GenreDB()},
     'artist' : {'schema':ArtistSchema(),'obj':ArtistDB()},
     'song' : {'schema':SongSchema(),'obj':SongDB()},
-    'year' : {'schema':YearSchema(),'obj':YearDB()}
+    'year' : {'schema':YearSchema(),'obj':YearDB()},
+    'songtime' : {'schema':SongTimeSchema(),'obj':SongTimeDB()},
 }
-
-DeclarativeBase.metadata.drop_all(SQL_cnx.engine)
-DeclarativeBase.metadata.create_all(SQL_cnx.engine)
 
 prepopulate.prepopulate_db(SQL_cnx, sql_tables)
 '''
